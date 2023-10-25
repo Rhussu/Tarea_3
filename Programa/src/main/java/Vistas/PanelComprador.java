706f060abd250ce4533772f858c5429aa1dd079f
Rhussu <wzaratesolar@gmail.com>
2023-10-25 04:00:53 -0300

    Agregados los métodos paints de las clases que componen a PanelComprador

--- Programa/src/main/java/Vistas/PanelComprador.java
+++ Programa/src/main/java/Vistas/PanelComprador.java
@@ -1,6 +1,19 @@
 package Vistas;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PanelComprador extends JPanel {
+	private PanelBilletera pb;
+	private PanelInventario pi;
+	public PanelComprador() {
+		pb = new PanelBilletera();
+		pi = new PanelInventario();
+	}
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		pb.paint(g);
+		pi.paint(g);
+	}
 }
--- Programa/src/main/java/Vistas/PanelPrincipal.java
+++ Programa/src/main/java/Vistas/PanelPrincipal.java
@@ -1,20 +1,28 @@
 package Vistas;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class PanelPrincipal extends JPanel {//se ve en el centro de la ventana
+public class PanelPrincipal extends JPanel {
     private PanelComprador com;
     private PanelExpendedor exp;
+
     public PanelPrincipal () {
         exp = new PanelExpendedor();
         com = new PanelComprador();
-        this.setBackground(Color.white);
+        this.setBackground(Color.WHITE);
     }
 
-    public void paint (Graphics g) { //todo se dibuja a partir de este método
-        super.paint(g); //llama al método pint al que hace override en la super clase
-        //el de la super clase solo pinta el fondo (background)
-        com.paint(g); //llama al metodo paint definido en el PanelComprador
-        exp.paint(g); //llama al metodo paint definido en el PanelExpendedor
+    public void paint (Graphics g) {
+        super.paint(g);
+        try {
+            ImageIcon imageIcon = new ImageIcon(getClass().getClassLoader().getResource("fondo.png"));
+            Image image = imageIcon.getImage();
+            g.drawImage(image, 0, 0,null);
+        } catch (Exception e) {
+            System.out.println("Error al cargar el fondo.");
+        }
+        com.paint(g);
+        exp.paint(g);
     }
 }

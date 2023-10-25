6db0e8d2b3c57c43b5e4d25cbb0193e62cef9f52
Rhussu <wzaratesolar@gmail.com>
2023-10-25 03:58:21 -0300

    Implementada representación ¡momentanea! del expendedor

--- Programa/src/main/java/Vistas/PanelExpendedor.java
+++ Programa/src/main/java/Vistas/PanelExpendedor.java
@@ -1,6 +1,31 @@
 package Vistas;
 
 import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
 
 public class PanelExpendedor extends JPanel {
+    @Override
+    public void paint(Graphics g) {
+
+        Dimension tamañoExpendedor = new Dimension(500,626);
+
+        super.paint(g);
+        g.setColor(Color.DARK_GRAY);
+        try {
+            ImageIcon imageIcon = new ImageIcon(getClass().getClassLoader().getResource("expendedor.png"));
+            Image image = imageIcon.getImage();
+            g.drawImage(image, (int) ((frameX() - tamañoExpendedor.getWidth()) / 2), (int) ((frameY() - tamañoExpendedor.getHeight()) / 2),null);
+        } catch (Exception e) {
+            System.out.println("Error al cargar la imagen del expendedor.");
+            g.fillRect((int) ((frameX() - tamañoExpendedor.getWidth()) / 2),(int) ((frameY() - tamañoExpendedor.getHeight()) / 2),500,625);
+        }
+    }
+
+    private int frameY() {
+        return (int) ((Ventana.frame().windowSize().getHeight()));
+    }
+    private int frameX() {
+        return (int) ((Ventana.frame().windowSize().getWidth()));
+    }
 }

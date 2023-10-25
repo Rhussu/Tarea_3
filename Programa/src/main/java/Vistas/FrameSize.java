77a31e10fb8d3aa6c633a5856aeec50f3e28acb5
Rhussu <wzaratesolar@gmail.com>
2023-10-25 19:31:05 -0300

    Overloading de los métodos de la interfaz FrameSize

--- Programa/src/main/java/Vistas/FrameSize.java
+++ Programa/src/main/java/Vistas/FrameSize.java
@@ -1,8 +1,24 @@
 package Vistas;
 
 public interface FrameSize {
+    default int frameY(double multiplicador,int agregado) {
+        return (int) (Ventana.frame().windowSize().getHeight() * multiplicador) + agregado;
+    }
+
+    default int frameY(int agregado) {
+        return (int) Ventana.frame().windowSize().getHeight() + agregado;
+    }
+
     default int frameY() {
         return (int) Ventana.frame().windowSize().getHeight();
+    }
+
+    default int frameX(double multiplicador, int agregado) {
+        return (int) (Ventana.frame().windowSize().getWidth() * multiplicador) + agregado;
+    }
+
+    default int frameX(int agregado) {
+        return (int) Ventana.frame().windowSize().getWidth() + agregado;
     }
 
     default int frameX() {

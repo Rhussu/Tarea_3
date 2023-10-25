cffb3e0ef47172ea122536d77d3c002f18be976a
Rhussu <wzaratesolar@gmail.com>
2023-10-25 19:33:44 -0300

    Implementada interfaz FrameSize en PanelBilletera

--- Programa/src/main/java/Vistas/PanelBilletera.java
+++ Programa/src/main/java/Vistas/PanelBilletera.java
@@ -3,7 +3,7 @@ package Vistas;
 import javax.swing.*;
 import java.awt.*;
 
-public class PanelBilletera extends JPanel {
+public class PanelBilletera extends JPanel implements FrameSize{
     public PanelBilletera() {
     }
     @Override
@@ -12,12 +12,12 @@ public class PanelBilletera extends JPanel {
         int distanciaAlBordeIzquierdo = 25;
 
         super.paint(g);
-        g.setColor(new Color(130,130,130,100));
+        g.setColor(new Color(130,110,110,100));
         int[] xp = {0,200,150,0};
         int[] yp = {0,0,80,80};
         g.fillPolygon(xp,yp,4);
-        g.setColor(new Color(180,180,180,100));
-        g.fillRect(0,80,150,(int) Ventana.frame().windowSize().getHeight() -80);
+        g.setColor(new Color(180,160,160,100));
+        g.fillRect(0,80,150,frameY(-80));
         ImageIcon[] imageIcon = new ImageIcon[6];
         Image[] image = new Image[6];
         try {
@@ -29,14 +29,10 @@ public class PanelBilletera extends JPanel {
             imageIcon[5] = new ImageIcon(getClass().getClassLoader().getResource("ayuda.png"));
             for(int i = 0; i < 6; i++) {
                 image[i] = imageIcon[i].getImage();
-                g.drawImage(image[i],distanciaAlBordeIzquierdo,frameX()*i,null);
+                g.drawImage(image[i],distanciaAlBordeIzquierdo,frameY(i/6.2,0),null);
             }
         } catch (Exception e) {
             System.out.println("Error al cargar las imágenes de las monedas.");
         }
-    }
-
-    private int frameX() {
-        return (int) ((Ventana.frame().windowSize().getHeight())/6.2);
     }
 }

fb88c84a36fa473b063bf0a4653ae12d3bc1492e
Rhussu <wzaratesolar@gmail.com>
2023-10-25 20:27:48 -0300

    Implementados dibujos de emergencia en caso de error con las imagenes

--- Programa/src/main/java/Vistas/PanelInventario.java
+++ Programa/src/main/java/Vistas/PanelInventario.java
@@ -3,25 +3,30 @@ package Vistas;
 import javax.swing.*;
 import java.awt.*;
 
-public class PanelInventario extends JPanel implements FrameSize{
+public class PanelInventario extends JPanel implements FrameSize {
     public PanelInventario() {
     }
     @Override
     public void paint(Graphics g) {
+        super.paint(g);
 
+        // Variables de ajuste
         int distanciaAlBordeDerecho = 70;
 
-        super.paint(g);
+        // Poligono semitransparente
         g.setColor(new Color(130,130,130,100));
-        int[] xp = {frameX(-200),frameX(),frameX(),frameX(-150)};
+        int[] xp = {frameWidth(-200),frameWidth(),frameWidth(),frameWidth(-150)};
         int[] yp = {0,0,80,80};
         g.fillPolygon(xp,yp,4);
         g.setColor(new Color(180,180,180,100));
-        g.fillRect(frameX(-150),80,150,frameX(-80));
+        g.fillRect(frameWidth(-150),80,150,frameWidth(-80));
+
+        // Agregado de imagenes
         ImageIcon[] imageIcon = new ImageIcon[3];
         Image[] image = new Image[3];
         try {
             imageIcon[0] = new ImageIcon(getClass().getClassLoader().getResource("inventario.png"));
+            // Debe ser remplazado por el ajuste real
             /*switch (depositounico) {
                 case COCA:
                     imageIcon[1] = new ImageIcon(getClass().getClassLoader().getResource("coca.png"));
@@ -44,13 +49,21 @@ public class PanelInventario extends JPanel implements FrameSize{
             for(int i = 0; i < 3; i++) {
                 image[i] = imageIcon[i].getImage();
                 if(i == 0){
-                    g.drawImage(image[i],frameX(-55 -distanciaAlBordeDerecho),0,null);
+                    g.drawImage(image[i],frameWidth(-55 -distanciaAlBordeDerecho),0,null);
                 } else {
-                    g.drawImage(image[i], frameX(-55 - distanciaAlBordeDerecho), frameY(i/2.6,-130), null);
+                    g.drawImage(image[i], frameWidth(-55 - distanciaAlBordeDerecho), frameHeight(i/2.6,-160), null);
                 }
             }
         } catch (Exception e) {
             System.out.println("Error al cargar las imágenes del inventario.");
+
+            // Represetación de emergencia
+            g.setColor(new Color(180,120,120));
+            g.setFont(new Font("Arial", Font.PLAIN, 20));
+            g.drawString("Inventario",frameWidth(-55 - distanciaAlBordeDerecho),50);
+            for(int i = 1; i < 3; i++) {
+                g.fillRect(frameWidth(-55 - distanciaAlBordeDerecho), frameHeight(i/2.6,-160), 100,200);
+            }
         }
     }
 }

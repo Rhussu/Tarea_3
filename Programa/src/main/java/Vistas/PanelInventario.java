eb41297326093269415611c67b2ff366bb3c682c
Rhussu <wzaratesolar@gmail.com>
2023-10-28 04:41:32 -0300

    Agregado el Inventario

--- Programa/src/main/java/Vistas/PanelInventario.java
+++ Programa/src/main/java/Vistas/PanelInventario.java
@@ -17,6 +17,36 @@ public class PanelInventario extends JPanel {
             ImageIcon imageIcon = new ImageIcon(getClass().getClassLoader().getResource("FondoInventario.png"));
             Image image = imageIcon.getImage();
             g.drawImage(image,0,0,null);
+            imageIcon = new ImageIcon(getClass().getClassLoader().getResource("BolsaMon.png"));
+            image = imageIcon.getImage();
+            g.drawImage(image,110,540,null);
+            switch(PanelComprador.getComprador().queConsumiste()) {
+                case "sprite":
+                    imageIcon = new ImageIcon(getClass().getClassLoader().getResource("Sprite.png"));
+                    image = imageIcon.getImage();
+                    g.drawImage(image,110,140,null);
+                    break;
+                case "cocacola":
+                    imageIcon = new ImageIcon(getClass().getClassLoader().getResource("Coca.png"));
+                    image = imageIcon.getImage();
+                    g.drawImage(image,110,140,null);
+                    break;
+                case "fanta":
+                    imageIcon = new ImageIcon(getClass().getClassLoader().getResource("Fanta.png"));
+                    image = imageIcon.getImage();
+                    g.drawImage(image,110,140,null);
+                    break;
+                case "snickers":
+                    imageIcon = new ImageIcon(getClass().getClassLoader().getResource("Snick.png"));
+                    image = imageIcon.getImage();
+                    g.drawImage(image,110,140,null);
+                    break;
+                case "super8":
+                    imageIcon = new ImageIcon(getClass().getClassLoader().getResource("Super8.png"));
+                    image = imageIcon.getImage();
+                    g.drawImage(image,110,140,null);
+                    break;
+            }
             System.out.println("Inventario cargado con exito");
         } catch (Exception e) {
             System.out.println("Error al cargar el Inventario");

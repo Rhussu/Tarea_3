b4238fda3e3aae4a7af122342c513271a008109c
Rhussu <wzaratesolar@gmail.com>
2023-10-28 03:50:50 -0300

    Agregadas las monedas al PanelComprador

--- Programa/src/main/java/Vistas/PanelBilletera.java
+++ Programa/src/main/java/Vistas/PanelBilletera.java
@@ -4,10 +4,30 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PanelBilletera extends JPanel {
+    private ButtonMoneda moneda100;
+    private ButtonMoneda moneda500;
+    private ButtonMoneda moneda1000;
+    private ButtonMoneda moneda1500;
     public PanelBilletera() {
         super();
+        moneda100 = new ButtonMoneda(100);
+        moneda500 = new ButtonMoneda(500);
+        moneda1000 = new ButtonMoneda(1000);
+        moneda1500 = new ButtonMoneda(1500);
+
+        moneda1500.setBounds(18,140,127,127);
+        moneda1000.setBounds(18,270,127,127);
+        moneda500.setBounds(18,400,127,127);
+        moneda100.setBounds(18,530,127,127);
+
         setOpaque(false);
+        setLayout(null);
         setBounds(0,0,260,Ventana.ALTO);
+
+        add(moneda100);
+        add(moneda500);
+        add(moneda1000);
+        add(moneda1500);
     }
     @Override
     public void paintComponent(Graphics g) {

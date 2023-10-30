1c1903443fc425bce265b7f4abd9b458068988bb
Rhussu <wzaratesolar@gmail.com>
2023-10-30 20:35:58 -0300

    Nueva variable estatica en ButtonMiProducto

--- Programa/src/main/java/Vistas/ButtonMiProducto.java
+++ Programa/src/main/java/Vistas/ButtonMiProducto.java
@@ -6,6 +6,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class ButtonMiProducto extends JButton {
+    public static boolean vacio = true;
     public ButtonMiProducto() {
         super();
         setOpaque(false);
@@ -22,45 +23,29 @@ public class ButtonMiProducto extends JButton {
 
             @Override
             public void mousePressed(MouseEvent e) {
-                try {
-                    if(PanelExpendedor.getExpendedor().getProducto() != null) {
-                        setIcon(null);
-                    }
-                } catch(NullPointerException f) {
-                    System.out.println("Expendedor no iniciado");
+                if(!vacio) {
+                    setIcon(null);
                 }
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
-                try {
-                    if(PanelExpendedor.getExpendedor().getProducto() != null) {
-                        setIcon(new ImageIcon(getClass().getClassLoader().getResource(/*Porque no habia una espesifica use FantaPress*/"FantaPress.png")));
-                    }
-                } catch(NullPointerException f) {
-                    System.out.println("Expendedor no iniciado");
+                if(!vacio) {
+                    setIcon(new ImageIcon(getClass().getClassLoader().getResource(/*Porque no habia una espesifica use FantaPress*/"FantaPress.png")));
                 }
             }
 
             @Override
             public void mouseEntered(MouseEvent e) {
-                try {
-                    if(PanelExpendedor.getExpendedor().getProducto() != null) {
-                        setIcon(new ImageIcon(getClass().getClassLoader().getResource(/*Porque no habia una espesifica use FantaPress*/"FantaPress.png")));
-                    }
-                } catch(NullPointerException f) {
-                    System.out.println("Expendedor no iniciado");
+                if(!vacio) {
+                    setIcon(new ImageIcon(getClass().getClassLoader().getResource(/*Porque no habia una espesifica use FantaPress*/"FantaPress.png")));
                 }
             }
 
             @Override
             public void mouseExited(MouseEvent e) {
-                try {
-                    if(PanelExpendedor.getExpendedor().getProducto() != null) {
-                        setIcon(null);
-                    }
-                } catch(NullPointerException f) {
-                    System.out.println("Expendedor no iniciado");
+                if(!vacio) {
+                    setIcon(null);
                 }
             }
         });

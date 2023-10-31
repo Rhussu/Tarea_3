709e2f842a3134afbf9e3f8bd143d2b6685b54ce
Angie161 <aramirez2022@udec.cl>
2023-10-31 12:39:44 -0300

    Agregado de documentación en clases Aplicacion y ButtonCancel

--- Programa/src/main/java/Vistas/Aplicacion.java
+++ Programa/src/main/java/Vistas/Aplicacion.java
@@ -1,6 +1,13 @@
 package Vistas;
 
+/**
+ * Clase para la ejecución de nuestra interfaz gráfica.
+ */
 public class Aplicacion {
+    /**
+     * Ejecuta la clase Ventana.
+     * @param args por defecto.
+     */
     public static void main(String[] args) {
         Ventana.frame();
     }
--- Programa/src/main/java/Vistas/ButtonCancel.java
+++ Programa/src/main/java/Vistas/ButtonCancel.java
@@ -5,7 +5,13 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+/**
+ * Clase que genera un botón de "cancelar" en el expendedor, el cual al presionarlo permite cancelar la compra que se iba a realizar.
+ */
 public class ButtonCancel extends JButton {
+    /**
+     * Constructor de ButtonCancel, configura los parámetros iniciales para que se muestren en pantalla.
+     */
     public ButtonCancel() {
         super();
         setOpaque(false);
@@ -15,9 +21,18 @@ public class ButtonCancel extends JButton {
         setBackground(new Color(0, 0, 0, 0));
         setBounds(303,310,84,40);
         addMouseListener(new MouseListener() {
+
+            /**
+             * Evento ejecutado al cliquear el botón. No se realiza implementación, pues no es requerido.
+             * @param e evento a ser procesado
+             */
             @Override
             public void mouseClicked(MouseEvent e) {}
 
+            /**
+             * Evento ejecutado al presionar el botón. Genera una compra con un producto nulo para que se almacene la moneda en el almacén de vuelto.
+             * @param e evento a ser procesado
+             */
             @Override
             public void mousePressed(MouseEvent e) {
                 setIcon(null);
@@ -34,16 +49,28 @@ public class ButtonCancel extends JButton {
                 }
             }
 
+            /**
+             * Evento ejecutado al soltar el botón. Muestra una imagen del botón presionado para generar el efecto de activación.
+             * @param e evento a ser procesado
+             */
             @Override
             public void mouseReleased(MouseEvent e) {
                 setIcon(new ImageIcon(getClass().getClassLoader().getResource("CancelPress.png")));
             }
 
+            /**
+             * Evento ejecutado al entrar el botón. Muestra una imagen del botón presionado para generar el efecto de activación.
+             * @param e evento a ser procesado
+             */
             @Override
             public void mouseEntered(MouseEvent e) {
                 setIcon(new ImageIcon(getClass().getClassLoader().getResource("CancelPress.png")));
             }
 
+            /**
+             * Evento ejecutado al salir del botón. Muestra
+             * @param e evento a ser procesado
+             */
             @Override
             public void mouseExited(MouseEvent e) {
                 setIcon(null);

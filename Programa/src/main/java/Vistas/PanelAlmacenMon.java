4cc83d4c28a053812fdbe4443ab3dfbbc117bc4b
Angie161 <aramirez2022@udec.cl>
2023-10-31 01:37:20 -0300

    Implementada la clase PanelAlmacenMon

---
+++ Programa/src/main/java/Vistas/PanelAlmacenMon.java
@@ -0,0 +1,43 @@
+package Vistas;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelAlmacenMon extends JPanel {
+
+    public PanelAlmacenMon() {
+        super();
+        setOpaque(false);
+        setBounds(50, 471, 171, 39);
+
+    }
+
+    private Image ImagenMon(int valor){
+        switch (valor){
+            case 100:
+                return new ImageIcon(getClass().getClassLoader().getResource("Lado100.png")).getImage();
+            case 500:
+                return new ImageIcon(getClass().getClassLoader().getResource("Lado500.png")).getImage();
+            case 1000:
+                return new ImageIcon(getClass().getClassLoader().getResource("Lado1000.png")).getImage();
+            case 1500:
+                return new ImageIcon(getClass().getClassLoader().getResource("Lado1500.png")).getImage();
+        }
+        System.out.println("Arroja imagen nula");
+        return null;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        try{
+
+            for (int i=0; i<PanelExpendedor.getExpendedor().verTamañoDep("monAlmacen"); i++){
+                System.out.println("Se dibuja vuelto mini");
+                g.drawImage(ImagenMon(PanelExpendedor.getExpendedor().verMoneda(i,false).getValor()),1+10*i,5, null);
+            }
+        } catch (Exception e){
+            System.err.println(e);
+        }
+    }
+}

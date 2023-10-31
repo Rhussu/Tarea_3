d15d81b32dda57775cf720a1b90c794d4b9e7ab6
Rhussu <wzaratesolar@gmail.com>
2023-10-31 17:55:44 -0300

    Cambio en la ubicación de la variable serie en los productos

--- Programa/src/main/java/Modelos/Producto.java
+++ Programa/src/main/java/Modelos/Producto.java
@@ -4,6 +4,7 @@ package Modelos;
  * Esta es la super-superclase de todos los productos del programa (Cocacola, Sprite, Fanta, Snickers, Super8) y es para dar la base a estos.
  */
 public abstract class Producto {
+    protected int serie;
 
     /**
      * Constructor por defecto.
@@ -16,4 +17,13 @@ public abstract class Producto {
      * Método abstracto que será utilizado por todas las sub-subclases para representar lo que son.
      */
     public abstract String consumir();
+
+    /**
+     * Este es un método getter para obtener el número de serie.
+     *
+     * @return La serie del producto.
+     */
+    public int getSerie() {
+        return serie;
+    }
 }

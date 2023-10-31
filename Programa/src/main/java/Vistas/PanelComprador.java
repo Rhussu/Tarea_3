6081cb2121705adde060b1d30f87ba0c51bcda7e
Rhussu <wzaratesolar@gmail.com>
2023-10-31 18:30:04 -0300

    Documentado PanelComprador

--- Programa/src/main/java/Vistas/PanelComprador.java
+++ Programa/src/main/java/Vistas/PanelComprador.java
@@ -5,10 +5,17 @@ import Modelos.*;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Panel que representa la perspectiva primera persona de nuestro comprador.
+ */
 public class PanelComprador extends JPanel {
 	private static Comprador comprador;
 	private PanelBilletera panelBilletera;
 	private PanelInventario panelInventario;
+
+	/**
+	 * Constructor personalizado en el que le doy características al panel.
+	 */
 	public PanelComprador() {
 		super();
 		comprador = new Comprador();
@@ -21,6 +28,11 @@ public class PanelComprador extends JPanel {
 		add(panelInventario);
 	}
 
+	/**
+	 * Getter de la variable comprador.
+	 *
+	 * @return El comprador.
+	 */
 	public static Comprador getComprador() {
 		return comprador;
 	}

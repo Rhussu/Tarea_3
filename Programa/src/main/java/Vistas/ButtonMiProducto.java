b5d71fd82b0d5815dc6e17d69d84e9b5d6b2e507
Angie161 <aramirez2022@udec.cl>
2023-10-31 01:44:14 -0300

    Avance en clase ButtonMiProducto

--- Programa/src/main/java/Vistas/ButtonMiProducto.java
+++ Programa/src/main/java/Vistas/ButtonMiProducto.java
@@ -22,10 +22,7 @@ public class ButtonMiProducto extends JButton {
             @Override
             public void mouseClicked(MouseEvent e) {
                 //Aqui va lo que ocurre al precionarlo
-                if(!vacio) {
-                    producto = PanelExpendedor.getExpendedor().getProducto();
-                    Ventana.frame().repaint();
-                }
+
             }
 
             @Override
@@ -60,24 +57,28 @@ public class ButtonMiProducto extends JButton {
 
     @Override
     public void paintComponent(Graphics g) {
-        if(!vacio) {
-            switch (producto.consumir()) {
-                case "cocacola":
-                    g.drawImage((new ImageIcon(getClass().getClassLoader().getResource("MiniCoca.png"))).getImage(),5,5,null);
-                    break;
-                case "sprite":
-                    g.drawImage((new ImageIcon(getClass().getClassLoader().getResource("MiniSprite.png"))).getImage(),5,5,null);
-                    break;
-                case "fanta":
-                    g.drawImage((new ImageIcon(getClass().getClassLoader().getResource("MiniFanta.png"))).getImage(),5,5,null);
-                    break;
-                case "super8":
-                    g.drawImage((new ImageIcon(getClass().getClassLoader().getResource("MiniSuper8.png"))).getImage(),5,5,null);
-                    break;
-                case "snicker":
-                    g.drawImage((new ImageIcon(getClass().getClassLoader().getResource("MiniSnicker.png"))).getImage(),5,5,null);
-                    break;
+        try {
+            if (!vacio) {
+                switch (producto.consumir()) {
+                    case "cocacola":
+                        g.drawImage((new ImageIcon(getClass().getClassLoader().getResource("MiniCoca.png"))).getImage(), 5, 5, null);
+                        break;
+                    case "sprite":
+                        g.drawImage((new ImageIcon(getClass().getClassLoader().getResource("MiniSprite.png"))).getImage(), 5, 5, null);
+                        break;
+                    case "fanta":
+                        g.drawImage((new ImageIcon(getClass().getClassLoader().getResource("MiniFanta.png"))).getImage(), 5, 5, null);
+                        break;
+                    case "super8":
+                        g.drawImage((new ImageIcon(getClass().getClassLoader().getResource("MiniSuper8.png"))).getImage(), 5, 5, null);
+                        break;
+                    case "snicker":
+                        g.drawImage((new ImageIcon(getClass().getClassLoader().getResource("MiniSnicker.png"))).getImage(), 5, 5, null);
+                        break;
+                }
             }
+        }catch (Exception e){
+            System.err.println(e);
         }
     }
 }

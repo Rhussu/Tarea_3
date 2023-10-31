ef3465371221115baadb400a6991e1452488f370
Angie161 <aramirez2022@udec.cl>
2023-10-31 01:31:58 -0300

    Implementación de funcionalidad de ButtonComprar

--- Programa/src/main/java/Vistas/ButtonComprar.java
+++ Programa/src/main/java/Vistas/ButtonComprar.java
@@ -1,11 +1,17 @@
 package Vistas;
 
+import Modelos.NoHayProductoException;
+import Modelos.PagoIncorrectoException;
+import Modelos.PagoInsuficienteException;
+import Modelos.*;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class ButtonComprar extends JButton {
+    public static TipoProducto tipoProducto = null;
     public ButtonComprar() {
         super();
         setOpaque(false);
@@ -16,13 +22,28 @@ public class ButtonComprar extends JButton {
         setBounds(304,240,80,50);
         addMouseListener(new MouseListener() {
             @Override
-            public void mouseClicked(MouseEvent e) {
-                //Aqui va lo que ocurre al precionarlo
-            }
+            public void mouseClicked(MouseEvent e) {}
 
             @Override
             public void mousePressed(MouseEvent e) {
                 setIcon(null);
+                if(ButtonMiProducto.vacio) try {
+
+                    PanelComprador.setComprador(PanelIngresoMon.getMonPago(), tipoProducto);
+                    if(!ButtonMiProducto.vacio) {
+                        ButtonMiProducto.producto = PanelExpendedor.getExpendedor().getProducto();
+                        ButtonMiProducto.vacio = false;
+                        Ventana.frame().repaint();
+                    }
+
+                } catch (Exception ex) {
+                    System.err.println(ex);
+                } finally {
+                    PanelIngresoMon.setMonPago(null);
+                    tipoProducto = null;
+
+                    Ventana.frame().repaint();
+                }
             }
 
             @Override

0161f3942b7648a6e7e5422eb4561c7981693eab
Angie161 <aramirez2022@udec.cl>
2023-10-31 12:38:54 -0300

    Agregado método para ver producto en clase Comprador

--- Programa/src/main/java/Modelos/Comprador.java
+++ Programa/src/main/java/Modelos/Comprador.java
@@ -4,8 +4,9 @@ package Modelos;
  * Esta clase simula un comprador que intenta comprar algo en la máquina expendedora con una moneda.
  */
 public class Comprador {
-    private String sonido;
+    private String sonido = null;
     private int vuelto=0;
+    private Producto producto = null;
 
     /**
      * Constructor por defecto del comprador.
@@ -26,10 +27,9 @@ public class Comprador {
     public void comprarProducto(Moneda moneda, TipoProducto cualProducto, Expendedor expendedor) throws PagoIncorrectoException, NoHayProductoException, PagoInsuficienteException {
         try {
             expendedor.comprarProducto(moneda, cualProducto);
-            sonido = expendedor.getProducto().consumir();
+            producto = expendedor.getProducto();
 
         } catch (PagoIncorrectoException | NoHayProductoException | PagoInsuficienteException e) {
-            sonido = null;
             throw e;
         }
     }
@@ -52,11 +52,22 @@ public class Comprador {
     }
 
     /**
+     * Método para consumir el producto comprado.
+     */
+    public void consumirMiProducto(){
+        sonido = producto.consumir();
+    }
+
+    /**
      * Este es un constructor personalizado y realiza la operación de comprar un producto en el expendedor.
      *
      * @return Un string que explica que se consumió.
      */
     public String queConsumiste(){
         return sonido;
+    }
+
+    public Producto verProducto (){
+        return producto;
     }
 }
